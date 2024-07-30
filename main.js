1d0c7a698ab5d97dac0554c8199ff9ece3ecf8fc
Metring <127319012+Metring@users.noreply.github.com>
2024-07-30 13:30:46 -0300

    m

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-const numeroSenha = document.querySelector('.parametro-')
+const numeroSenha = document.querySelector('.parametro-senha_texto')
+const botoes = document.querySelectorAll('.parametro-senha_botao')
+
+botoes[0].onclick = diminuirTamanho
+botoes[1].onclick = aumentarTamanho
+
+let tamanhoSenha = 5
+numeroSenha.textContent = tamanhoSenha
+
+function diminuirTamanho() {
+    console.log('teste');
+    tamanhoSenha = tamanhoSenha -1
+    numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentarTamanho(){
+    console.log('teste')
+    tamanhoSenha = tamanhoSenha +1
+    numeroSenha.textContent = tamanhoSenha
+}
