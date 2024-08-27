9ec41bc5df2d013ab7b68d62928a93d2dcb5be7f
Metring <127319012+Metring@users.noreply.github.com>
2024-08-27 07:46:08 -0300

    j

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const numeroSenha = document.querySelector('.parametro-senha_texto')
-const botoes = document.querySelectorAll('.parametro-senha_botao')
+
+
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox')
+
+console.log(checkbox[0].checked)
 
 botoes[0].onclick = diminuirTamanho
 botoes[1].onclick = aumentarTamanho
@@ -8,19 +14,55 @@ let tamanhoSenha = 5
 numeroSenha.textContent = tamanhoSenha
 
 let maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let minuscula = "abcdefghijklmnoprrstuvwxyz"
+let minuscula = "abcdefghijklmnopqrstuvwxyz"
 let numero = "0123456789"
-let simbolo = "!?@%&"
-console.log(maiuscula)
+let simbolos = "!?@%*&"
 
-function diminuirTamanho() {
-    if (tamanhoSenha > 1 )
-    tamanhoSenha = tamanhoSenha -1
+geraSenha()
+console.log(maiuscula);
+
+for(i=0; 1 < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha
+}
+
+function diminuirTamanho(){
+    if (tamanhoSenha > 1){
+    tamanhoSenha = tamanhoSenha - 1
     numeroSenha.textContent = tamanhoSenha
+    }
+    geraSenha()
 }
 
 function aumentarTamanho(){
-    if (tamanhoSenha < 20 )
-    tamanhoSenha = tamanhoSenha +1
+    if (tamanhoSenha < 20){
+    tamanhoSenha = tamanhoSenha + 1
     numeroSenha.textContent = tamanhoSenha
+    }
+    geraSenha()
+}
+
+function geraSenha(){
+    let alfabeto = "";
+    let senha = "";
+    if(checkbox[0].checked){
+        alfabeto = alfabeto + maiuscula
+    }
+    if(checkbox[1].checked){
+        alfabeto = alfabeto + minuscula
+    }
+    if(checkbox[2].checked){
+        alfabeto = alfabeto + numero
+    }
+    if(checkbox[3].checked){
+        alfabeto = alfabeto + simbolos
+    }
+
+    for(let i = 0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*alfabeto.length
+        numeroAleatorio = Math.floor(numeroAleatorio)
+        senha = senha + alfabeto[numeroAleatorio];
+
+        console.log(senha)
+    }
+    campoSenha.value = senha
 }
